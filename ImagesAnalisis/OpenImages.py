0bc445c053e51a122d9d1d316283f19ffa72ab2f
Carlos-ZRM <carloszrm90@gmail.com>
2018-10-09 11:46:39 -0500

    hola

--- ImagesAnalisis/OpenImages.py
+++ ImagesAnalisis/OpenImages.py
@@ -1,12 +1,61 @@
 from PIL import Image
+from time_series import Plot_Time_Series
 import sys
+
+def umbral (top, img):
+    datos = list(img.getdata())
+    aux =[]
+    sum = 0
+    img.show()
+    print (datos)
+    for i in range (len(datos)):
+        aux= datos[i]
+        sum = 0
+        for j in range (len(aux)):
+            sum+=aux[j]
+        sum=sum/len(aux)
+        if sum > top :
+            datos[i]=(0,0,0)
+        else :
+            datos[i]=(255,255,255)
+    result = datos
+    img.putdata(datos)
+    img.save("test.jpg")
+    img.show()
+    return result
+
+def escala ( img ):
+    datos = list(img.getdata())
+    aux =[]
+    sum = 0
+    escala = 0
+    img.show()
+    print (datos)
+    for i in range (len(datos)):
+        aux= datos[i]
+        sum = 000
+        for j in range (len(aux)):
+            sum+=aux[j]
+        sum=sum/len(aux)
+        escala = int (sum*.33 + sum*.5 + sum*.15)
+        datos[i]= (escala,escala,escala)
+    result = datos
+    img.putdata(datos)
+    img.save("test.jpg")
+    img.show()
+    return result
 
 print("Hola")
 path = sys.argv[1]
 path = "snakes.jpg"
 try:
-	img = Image.open(path)
-	print("Se abrio imagen")
-	print (img.histogram()) 
+    img = Image.open(path)
+    print("Se abrio imagen")
+    print (img.histogram())
+    escala( img )
+    umbral(3/5*255   ,img)
+    histograma=Plot_Time_Series()
+    histograma.plot_Histograma(img.histogram())
+
 except IOError:
     print("Error")
--- ImagesAnalisis/time_series.py
+++ ImagesAnalisis/time_series.py
@@ -2,9 +2,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
-from mpl_toolkits.mplot3d import Axes3D
 
 class Plot_Time_Series:
+
+    def __init__(self):
+        self.__id=0
     def read_file(self, path):
         f = open(path, 'r')
         str = f.read();
@@ -14,15 +16,25 @@ class Plot_Time_Series:
     def f(self, t):
         return np.exp(-t) * np.cos(2*np.pi*t)
 
-    def make_graf_str(self, string, spl, dt):
+    def make_graf_str(self, str, spl):
         str = file_r().split( spl )
         s1_aux = []
         for i in range(len(str)):
             s1_aux.append( int(str[i]))
-        t = np.arange(0,len(str)-1,dt)
+        #t = np.arange(0,len(str),1)
+        y = []
         x = np.array(s1_aux)
-        return x , y
-    def plot():
+        yield x
+        yield y
+    def make_graf_arr( self, array):
+        dt = 1
+        y = np.arange(0,len(array), 1)
+        x = np.array(array)
+        yield x
+        yield y
+
+    def plot_Histograma( self, array ):
+        x , y = self.make_graf_arr(array)
         fig, (ax1) = plt.subplots()
-        ax1.plot(t, np.array(s1))
+        ax1.plot( y , x )
         plt.show()

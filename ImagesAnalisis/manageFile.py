a528124fde14c6ae45ee9f6c2ac7f8fbb7b7161e
Carlos-ZRM <carloszrm90@gmail.com>
2018-11-12 00:45:57 -0600

    hacer directorio eca

--- ImagesAnalisis/manageFile.py
+++ ImagesAnalisis/manageFile.py
@@ -1,6 +1,13 @@
 from PIL import Image
 from CA import *
+from os import listdir
+from os.path import isfile, join
 
+
+def openDirectory (directory) :
+    list = [f for f in listdir(directory) if isfile(join(directory, f))]
+    print (list)
+    return list
 def openFile( file ):
     with open( file , "r") as f:
         list_lines = f.readlines() 
@@ -9,7 +16,6 @@ def makeImage( file, image ) :
     data = openFile (file)
     ca_obj = buildCA(data)
     dataxy = [ ca_obj.getRing(), ca_obj.getTimes() ]
-
     i = Image.new('1', dataxy)
     i.putdata( ca_obj.getData() )
     i.save(image)
@@ -23,8 +29,15 @@ def buildCA( line):
     
     for i in range ( 1, object.getTimes() ):
         for j in range ( len (line[i]) -1) :
-            data.append( int(line[i][j]) )
+            data.append( not int(line[i][j]) )
     object.setData( data )        
     return object
-    
-makeImage( "test.txt" , "testca.png" )
+def buildCADirectory(path) :
+    archivos = openDirectory(path)
+    for i in range ( len(archivos) ) :
+        if "Data" in archivos[i] :
+           makeImage(path+archivos[i], archivos[i][0:-4]+".png")  
+    return 
+#makeImage( "test.txt" , "testca.png" )
+openDirectory("64/")
+buildCADirectory("64/")
